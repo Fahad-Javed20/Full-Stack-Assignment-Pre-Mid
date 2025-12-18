caaaeb7852d3e650405bc25dfc79aa33d21a8d25
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-19 01:04:06 +0500

    making backend

--- backend/src/Config/db.ts
+++ backend/src/Config/db.ts
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "";
+const MONGODB_URL = process.env.MONGODB_URL || "";
 
 const connectDB = async () => {
   try {
-    if (!MONGODB_URI) {
+    if (!MONGODB_URL) {
       throw new Error("MONGODB_URI is not defined");
     }
 
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URL);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error("MongoDB Error:", err);
