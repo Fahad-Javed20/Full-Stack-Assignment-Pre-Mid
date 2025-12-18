05e4e6b4fdf20e54ee74b3d64b383fce789ae7e2
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-19 01:37:45 +0500

    Update CustomerController.ts

--- backend/src/controllers/CustomerController.ts
+++ backend/src/controllers/CustomerController.ts
@@ -3,31 +3,36 @@ import Customer from "../models/CustomerModel";
 
 class CustomerController {
   async getAllCustomers(req: Request, res: Response) {
-    Customer.find()
-      .then((customers) => res.json(customers))
-      .catch((err) => res.status(500).json({ error: err.message }));
+    try {
+      const customers = await Customer.find();
+      res.json(customers);
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
   }
 
   async createCustomer(req: Request, res: Response) {
-    const newCustomer = new Customer(req.body);
-    newCustomer.save()
-      .then((customer) => res.status(201).json(customer))
-      .catch((err) => res.status(400).json({ error: err.message }));
+    try {
+      const newCustomer = new Customer(req.body);
+      const customer = await newCustomer.save();
+      res.status(201).json(customer);
+    } catch (err: any) {
+      res.status(400).json({ error: err.message });
+    }
   }
 
   async getCustomerById(req: Request, res: Response) {
-    const id  = Number(req.params.customerId);
-
-    Customer.findById(id)
-      .then((customer) => {
-        if (!customer) {
-          return res.status(404).json({ error: "Customer not found" });
-        }
-        res.json(customer);
-      })
-      .catch((err) => res.status(500).json({ error: err.message }));
+    try {
+      const id = req.params.customerId; 
+      const customer = await Customer.findById(id);
+      if (!customer) {
+        return res.status(404).json({ error: "Customer not found" });
+      }
+      res.json(customer);
+    } catch (err: any) {
+      res.status(500).json({ error: err.message });
+    }
   }
 }
 
 export default CustomerController;
-
