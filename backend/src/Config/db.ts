551103e2137dd7fa5db09e42b507953c80fd2dee
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-19 01:11:45 +0500

    Update db.ts

--- backend/src/Config/db.ts
+++ backend/src/Config/db.ts
@@ -1,26 +1,15 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL || "";
+export const connectDB = async () => {
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined in .env");
+  }
 
-const connectDB = async () => {
   try {
-    if (!MONGODB_URL) {
-      throw new Error("MONGODB_URI is not defined");
-    }
-
-    await mongoose.connect(MONGODB_URL);
-    console.log("Connected to MongoDB");
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(" Connected to MongoDB");
   } catch (err) {
-    console.error("MongoDB Error:", err);
+    console.error(" MongoDB Connection Error:", err);
+    process.exit(1);
   }
 };
-
-const disconnectDB = async () => {
-  try {
-    await mongoose.disconnect();
-  } catch (err) {
-    console.error("MongoDB Error:", err);
-  }
-};
-
-export { connectDB, disconnectDB };
